219f4f88210e60231585baf8d65628476d49de3e
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2025-05-31 21:31:37 +0100

    script for asteroid

---
+++ asteroid.py
@@ -0,0 +1,16 @@
+import pygame
+from circleshape import CircleShape
+from constants import *
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        # Draw the asteroid as a circle
+        pygame.draw.circle(screen, (255, 255, 255),
+                           (int(self.position.x), int(self.position.y)), self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
