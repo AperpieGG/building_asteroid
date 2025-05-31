4228477169bf14756056da69b7a08e6356863193
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2025-05-31 12:44:18 +0100

    add requirements

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 def main():
     # make a while loop
     pygame.init()
+    pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -18,10 +20,11 @@ def main():
                 pygame.quit()
 
         # Fill screen with black
-        screen.fill('black') 
+        screen.fill('black')
 
         # Update display
         pygame.display.flip()
+        pygame.time.Clock.tick(60)
 
 
 if __name__ == "__main__":
